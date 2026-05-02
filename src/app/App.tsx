630e3266996cb9826e20ed5ad80a1108878f2ce0
MustangWu <jx010405@gmail.com>
2026-05-02 14:00:07 +1000

    fix: remove SessionContext dependency from iteration1 password gate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
-import { SessionProvider } from './context/SessionContext';
 import { PasswordGate } from './components/PasswordGate';
 
 export default function App() {
   return (
     <PasswordGate>
-      <SessionProvider>
-        <RouterProvider router={router} />
-      </SessionProvider>
+      <RouterProvider router={router} />
     </PasswordGate>
   );
 }
